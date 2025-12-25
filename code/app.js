6d75b85e75bb274f9f44a79e8190c2e0f13984ec
Lavanya nallabelli <lavanyanallabelli@gmail.com>
2025-12-24 21:58:19 -0500

    first commit

---
+++ code/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('App is running');
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
